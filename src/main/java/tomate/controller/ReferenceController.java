9e0d40e07ebb395a15461020f676de308a5e1e81
Toni Oksanen <toni.oksanen@cs.helsinki.fi>
2014-09-23 22:49:43 +0300

    testi committi

--- src/main/java/tomate/controller/ReferenceController.java
+++ src/main/java/tomate/controller/ReferenceController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tomate.domain.BookReference;
-import tomate.domain.Reference;
 import tomate.repository.ReferenceRepository;
 
 /**
@@ -21,7 +20,7 @@ public class ReferenceController {
   private ReferenceRepository referenceRepository;
   
   @RequestMapping( "/references" )
-  public Reference findReference() {
+  public BookReference findReference() {
     
     // testataan jenkinssiä
     
