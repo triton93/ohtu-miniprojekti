0d107d24858aaf1a3d4eefd0fc8e1ea16a12f1cc
Toni Oksanen <toni.oksanen@cs.helsinki.fi>
2014-09-24 02:24:56 +0300

    rest-api metodit

--- src/main/java/tomate/controller/ReferenceController.java
+++ src/main/java/tomate/controller/ReferenceController.java
@@ -1,8 +1,13 @@
 
 package tomate.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import tomate.domain.BookReference;
 import tomate.repository.ReferenceRepository;
@@ -13,16 +18,35 @@ import tomate.repository.ReferenceRepository;
  */
 
 @RestController
-@RequestMapping( "/api" )
+@RequestMapping( "/api/references" )
 public class ReferenceController {
   
   @Autowired
   private ReferenceRepository referenceRepository;
   
-  @RequestMapping( "/references" )
-  public BookReference findReference() {
-    return new BookReference();
+  @RequestMapping( method = RequestMethod.PUT )
+  public void create( @RequestBody BookReference reference ) {
+    
   }
   
+  @RequestMapping( value = "/{id}", method = RequestMethod.GET )
+  public BookReference retrieve( @PathVariable Long id ) {
+    return null;
+  }
+  
+  @RequestMapping( method = RequestMethod.GET )
+  public List<BookReference> list() {
+    return null;
+  }
+  
+  @RequestMapping( value = "/{id}", method = RequestMethod.DELETE )
+  public void delete( @PathVariable Long id ) {
+    
+  }
+  
+  @RequestMapping( value = "/{id}", method = RequestMethod.PUT )
+  public void update( @PathVariable Long id  ) {
+    
+  }
   
 }
