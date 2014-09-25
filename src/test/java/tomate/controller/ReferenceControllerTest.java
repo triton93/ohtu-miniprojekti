6645aae74fc24124d2f0b6e0880e19118e3c84eb
Toni Oksanen <toni.oksanen@cs.helsinki.fi>
2014-09-25 08:07:01 +0300

    junit testeja

--- src/test/java/tomate/controller/ReferenceControllerTest.java
+++ src/test/java/tomate/controller/ReferenceControllerTest.java
@@ -2,11 +2,18 @@
 package tomate.controller;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import tomate.Application;
 
 /**
  *
  * @author Toni Oksanen
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
 public class ReferenceControllerTest {
   
   
