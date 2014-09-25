a246d6aa6d36de05739138cbd3690e84f950de13
Toni Oksanen <toni.oksanen@cs.helsinki.fi>
2014-09-26 00:02:35 +0300

    restin integrointi

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -3,41 +3,80 @@ var app = angular.module( 'app', [ 'ui.router' ] );
 
 app.config(function ( $stateProvider ) {
   
-  $stateProvider
-    .state( 'add', {
-      url: '',
+  var views = {
+    addOrEditView: {
       templateUrl: 'views/add-ref-tmpl.html',
-      controller: 'RefCtrl'
-    })
-    .state( 'list', {
-      url: '/list',
+      controller: 'RefAddOrUpdateCtrl'
+    },
+    listView: {
       templateUrl: '/views/list-refs-tmpl.html',
-      controller: function ( $scope, $http ) {
-        
-        $http({
-          url: '/api/references',
-          method: 'GET'
-        })
-        .success(function ( refs ) {
-          $scope.refs = refs;
-        });
-      
-      }
+      controller: 'RefListCtrl'
+    }
+  };
+  
+  $stateProvider
+    .state( 'index', {
+      url: '',
+      views: views
+    })
+    .state( 'edit', {
+      url: '/{id}',
+      views: views
     });
 
 });
 
-
-
-app.controller( 'RefCtrl', function ( $scope, $http ) {
+app.controller( 'RefAddOrUpdateCtrl', function ( $scope, $http, $state ) {
   
-  $scope.addRef = function ( ref ) {
+  var isEdit = angular.isDefined( $state.params.id );
+  $scope.isEdit = isEdit;
+  
+  if ( isEdit ) {
+  
+    $http({
+      url: '/api/references/' + $state.params.id,
+      method: 'GET'
+    })
+    .success(function ( ref ) {
+      $scope.ref = ref;      
+    });
+  
+  }
+  
+  
+  $scope.saveRef = function ( ref ) {
+    
+    if ( isEdit ) {
+      delete ref.new;
+    }
+    
     $http({
       url: '/api/references',
       method: 'PUT',
       data: ref
+    })
+    .success(function () {
+      
+      if ( isEdit ) {
+         $state.transitionTo ('index', {}, { location: true })
+      } 
+      else {
+        $state.go( $state.current.name, $state.params, { reload: true } );
+      }
+    
     });
   };
 
 });
 
+app.controller( 'RefListCtrl', function ( $scope, $http ) {
+        
+  $http({
+    url: '/api/references',
+    method: 'GET'
+  })
+  .success(function ( refs ) {
+    $scope.refs = refs;
+  });
+
+});
