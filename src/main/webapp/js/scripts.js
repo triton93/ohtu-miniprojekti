7906f7c2ad71e0c089f37823023d4a9135a956d0
Toni Oksanen <toni.oksanen@cs.helsinki.fi>
2014-09-26 08:56:19 +0300

    alert lisaykseen ja poistoon

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -26,46 +26,53 @@ app.config(function ( $stateProvider ) {
 
 });
 
-app.controller( 'RefAddOrUpdateCtrl', function ( $scope, $http, $state ) {
+app.controller( 'RefAddOrUpdateCtrl', 
+  function ( $scope, $http, $state, $window ) {
   
-  var isEdit = angular.isDefined( $state.params.id );
-  $scope.isEdit = isEdit;
-  
-  if ( isEdit ) {
-  
-    $http({
-      url: '/api/references/' + $state.params.id,
-      method: 'GET'
-    })
-    .success(function ( ref ) {
-      $scope.ref = ref;      
-    });
-  
-  }
-  
-  
-  $scope.saveRef = function ( ref ) {
-    
+    var isEdit = angular.isDefined( $state.params.id );
+    $scope.isEdit = isEdit;
+
     if ( isEdit ) {
-      delete ref.new;
+
+      $http({
+        url: '/api/references/' + $state.params.id,
+        method: 'GET'
+      })
+      .success(function ( ref ) {
+        $scope.ref = ref;      
+      });
+
     }
-    
-    $http({
-      url: '/api/references',
-      method: 'PUT',
-      data: ref
-    })
-    .success(function () {
-      
+
+
+    $scope.saveRef = function ( ref ) {
+
       if ( isEdit ) {
-         $state.transitionTo ('index', {}, { location: true })
-      } 
-      else {
-        $state.go( $state.current.name, $state.params, { reload: true } );
+        delete ref.new;
       }
-    
-    });
-  };
+
+      $http({
+        url: '/api/references',
+        method: 'PUT',
+        data: ref
+      })
+      .success(function () {
+
+        if ( isEdit ) {
+          
+           $state.transitionTo ('index', {}, { location: true });
+           $window.alert( "Reference has been added! Thank you!" );
+           
+        } 
+        else {
+          
+          $state.go( $state.current.name, $state.params, { reload: true } );
+          $window.alert( "Reference has been saved! Thank you!" );
+        
+        }
+      
+      });
+    };
 
 });
 
