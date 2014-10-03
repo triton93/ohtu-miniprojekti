2ecf7619d331e92329995707802d00757d958c9a
Marko Laine <kheevo@gmail.com>
2014-10-03 09:36:00 +0300

    deleteref are you sure-popup

--- src/main/webapp/js/scripts.js
+++ src/main/webapp/js/scripts.js
@@ -1,127 +1,131 @@
 
-var app = angular.module( 'app', [ 'ui.router' ] );
+var app = angular.module('app', ['ui.router']);
 
-app.config(function ( $stateProvider ) {
-  
-  var commonViews = {
-    addOrEditView: {
-      templateUrl: 'views/add-ref-tmpl.html',
-      controller: 'RefAddOrUpdateCtrl'
-    },
-    listView: {
-      templateUrl: '/views/list-refs-tmpl.html',
-      controller: 'RefListCtrl'
-    }
-  };
-  
-  $stateProvider
-    .state( 'index', {
-      url: '',
-      views: commonViews
-    })
-    .state( 'edit', {
-      url: '/{id}',
-      views: commonViews
-    })
-    .state( 'preview', {
-      url: '/preview/{id}',
-      views: {
-        previewView: {
-          templateUrl: '/views/preview-tmpl.html',
-          controller: 'PreviewCtrl',
-          resolve: {
-            reference: function ( $stateParams, $http ) {
-              return $http({ 
-                url: '/api/references/' + $stateParams.id, 
-                method: 'GET' 
-              }).success(function ( resp ) {
-                return resp;
-              });
-            },
-            bibtex: function ( $stateParams, $http ) {
-              return $http({ 
-                url: '/api/bibtex/' + $stateParams.id, 
-                method: 'GET' 
-              }).success(function ( resp ) {
-                return resp;
-              });
-            }
-          }
+app.config(function($stateProvider) {
+
+    var commonViews = {
+        addOrEditView: {
+            templateUrl: 'views/add-ref-tmpl.html',
+            controller: 'RefAddOrUpdateCtrl'
+        },
+        listView: {
+            templateUrl: '/views/list-refs-tmpl.html',
+            controller: 'RefListCtrl'
         }
-      }
-    });
+    };
+
+    $stateProvider
+            .state('index', {
+                url: '',
+                views: commonViews
+            })
+            .state('edit', {
+                url: '/{id}',
+                views: commonViews
+            })
+            .state('preview', {
+                url: '/preview/{id}',
+                views: {
+                    previewView: {
+                        templateUrl: '/views/preview-tmpl.html',
+                        controller: 'PreviewCtrl',
+                        resolve: {
+                            reference: function($stateParams, $http) {
+                                return $http({
+                                    url: '/api/references/' + $stateParams.id,
+                                    method: 'GET'
+                                }).success(function(resp) {
+                                    return resp;
+                                });
+                            },
+                            bibtex: function($stateParams, $http) {
+                                return $http({
+                                    url: '/api/bibtex/' + $stateParams.id,
+                                    method: 'GET'
+                                }).success(function(resp) {
+                                    return resp;
+                                });
+                            }
+                        }
+                    }
+                }
+            });
 
 });
 
-app.controller( 'RefAddOrUpdateCtrl', 
-  function ( $scope, $http, $state ) {
-  
-    var isEdit = angular.isDefined( $state.params.id );
-    $scope.isEdit = isEdit;
+app.controller('RefAddOrUpdateCtrl',
+        function($scope, $http, $state) {
 
-    if ( isEdit ) {
+            var isEdit = angular.isDefined($state.params.id);
+            $scope.isEdit = isEdit;
 
-      $http({
-        url: '/api/references/' + $state.params.id,
-        method: 'GET'
-      })
-      .success(function ( ref ) {
-        $scope.ref = ref;      
-      });
+            if (isEdit) {
 
-    }
+                $http({
+                    url: '/api/references/' + $state.params.id,
+                    method: 'GET'
+                })
+                        .success(function(ref) {
+                            $scope.ref = ref;
+                        });
 
-    $scope.saveRef = function ( ref ) {
+            }
 
-      if ( isEdit ) {
-        delete ref.new;
-      }
+            $scope.saveRef = function(ref) {
 
-      $http({
+                if (isEdit) {
+                    delete ref.new;
+                }
+
+                $http({
+                    url: '/api/references',
+                    method: 'PUT',
+                    data: ref
+                })
+                        .success(function() {
+
+                            if (isEdit) {
+                                $state.transitionTo('index', {}, {location: true});
+                            }
+                            else {
+                                $state.go($state.current.name, $state.params, {reload: true});
+                            }
+
+                        });
+            };
+
+        });
+
+app.controller('RefListCtrl', function($scope, $http, $state) {
+
+    $http({
         url: '/api/references',
-        method: 'PUT',
-        data: ref
-      })
-      .success(function () {
+        method: 'GET'
+    })
+            .success(function(refs) {
+                $scope.refs = refs;
+            });
 
-        if ( isEdit ) {
-          $state.transitionTo ('index', {}, { location: true });
-        } 
-        else {
-          $state.go( $state.current.name, $state.params, { reload: true } );
+    $scope.deleteRef = function(ref) {
+
+        var delVal = confirm("Are you sure?");
+        if (delVal === true) {
+            $http({
+                url: '/api/references/' + ref.id,
+                method: 'DELETE'
+            })
+                    .success(function() {
+                        $state.go($state.current.name, $state.params, {reload: true});
+                    });
+
+
         }
-      
-      });
+        ;
     };
 
 });
 
-app.controller( 'RefListCtrl', function ( $scope, $http, $state ) {
-        
-  $http({
-    url: '/api/references',
-    method: 'GET'
-  })
-  .success(function ( refs ) {
-    $scope.refs = refs;
-  });
-  
-  $scope.deleteRef = function ( ref ) {
-      
-    $http({
-      url: '/api/references/' + ref.id,
-      method: 'DELETE'
-    })
-    .success(function () {
-      $state.go( $state.current.name, $state.params, { reload: true } );
-    });
-      
-   };
-  
-
-});
-
-app.controller( 'PreviewCtrl', function ( $scope, bibtex, reference ) {
-  $scope.ref = reference.data;
-  $scope.bibtex = bibtex.data;
+app.controller('PreviewCtrl', function($scope, bibtex, reference) {
+    $scope.ref = reference.data;
+    $scope.bibtex = bibtex.data;
 });
