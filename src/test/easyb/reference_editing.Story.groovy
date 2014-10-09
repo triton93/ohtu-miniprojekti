10bfc647b576c7bbc5afb6611b92373dc741117d
Marko Laine <kheevo@gmail.com>
2014-10-09 19:22:26 +0300

    easyb

---
+++ src/test/easyb/reference_editing.Story.groovy
@@ -0,0 +1,22 @@
+import tomate.*
+import org.openqa.selenium.*
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+
+description 'User can edit saved book reference'
+scenario "User edits reference successfully and saves the changes" {
+given 'user starts to edit a reference', {
+}
+when 'user submits valid data to all the data fields', {
+}
+then 'reference will be updated and saved to repository', {
+}
+}
+scenario "User cant submit changes if one or more data items is left blank" {
+given 'user starts to edit a reference', {
+}
+when 'user doesnt submit valid data to all the data fields', {
+}
+then 'reference will not be updated', {
+}
+}
--- src/test/easyb/reference_listing.story
+++
@@ -1,7 +0,0 @@
-description 'User can view list of references'
-
-scenario "User can view all the saved references" {
-    given 'user visits main page'
-    when 'main page is loaded'
-    then 'reference list is shown'
-}
